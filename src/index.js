15b28b69fac66e2ea2fca792cceaec8134c84aaf
srikanth <srikanth@digio.in>
2021-09-21 16:05:54 +0530

    dragholder working initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+let sidebar = document.querySelector(".sidebar");
+let content = document.querySelector(".content");
+let dragholder = document.querySelector(".dragholder");
+
+function onMouseMove(e){
+  sidebar.style.cssText = `width: ${ e.pageX }px`;
+  content.style.cssText = `width: ${ window.innerWidth - e.pageX }px`;
+}
+
+function onMouseDown(e){
+  document.addEventListener('mousemove',onMouseMove);
+}
+
+function onMouseUp(e){
+  document.removeEventListener('mousemove',onMouseMove);
+}
+
+dragholder.addEventListener('mousedown', onMouseDown);
+dragholder.addEventListener('mouseup', onMouseUp);
+content.addEventListener('mouseup', onMouseUp);
+document.addEventListener('mouseup', onMouseUp);
